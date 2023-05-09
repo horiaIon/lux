d3bc4168e8e010bcf261dee23bbe139b1747701e
Horia Ion <horia.ion@picsart.com>
2023-05-09 09:43:57 +0300

    prod release

--- src/app/app.settings.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Settings } from './app.settings.model';
-
-@Injectable()
-export class AppSettings {
-  settings = new Settings('LauraP-UX', {
-    showMenu: true,
-  });
-}
